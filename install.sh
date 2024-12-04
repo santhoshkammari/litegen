bde78ec57e7f4f5050c18bf32411c6f3710c40ea
kammarisanthosh <kammari.santhosh@newgensoft.com>
2024-12-04 20:05:21 +0530

    updated install

--- install.sh
+++ install.sh
@@ -2,21 +2,6 @@
 
 set -e
 
-# Git operations
-echo "Performing Git operations..."
-git add .
-if git diff --staged --quiet; then
-    echo "No changes to commit"
-else
-    git commit -m "Updated files before deployment"
-    git push
-fi
-
-# Check if dist directory exists before deleting
-if [ -d "dist" ]; then
-    echo "Removing existing dist directory..."
-    rm -r dist
-fi
 
 echo "Uninstalling existing package..."
 pip uninstall -y ailite
