fb23418c9666214a3d23914690cbedc5b25f3acf
santhoshkammari <santhoshkammari1999@gmail.com>
2024-12-31 12:11:07 +0530

    optimize code

--- test/main.py
+++ test/main.py
@@ -1,5 +1,7 @@
-from litegen import completion, pp_completion
+from litegen import completion
 
-pp_completion(model='exaone3.5:2.4b',
+res = completion(model='exaone3.5:2.4b',
               messages="tell me about narendra modi in 5 paragrapsh simple",
               gpu=True)
+
+print(res.choices[0].message.content)
