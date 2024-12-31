ebc0a761adae871115c26d12eb56238fc0820a4b
santhoshkammari <santhoshkammari1999@gmail.com>
2024-12-31 12:12:39 +0530

    optimize code

--- test/main.py
+++ test/main.py
@@ -1,7 +1,7 @@
 from litegen import completion
 
 res = completion(model='exaone3.5:2.4b',
-              messages="tell me about narendra modi in 5 paragrapsh simple",
-              gpu=True)
+                 messages="tell me about narendra modi in 5 paragrapsh simple",
+                 gpu=True)
 
 print(res.choices[0].message.content)
