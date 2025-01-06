871d3f273c44269db59ff3f7ce4955c2d3f1011f
santhoshkammari <santhoshkammari1999@gmail.com>
2025-01-06 13:05:22 +0530

    update test

--- test/dsollama_.py
+++ test/dsollama_.py
@@ -4,6 +4,6 @@ import os
 os.environ['OPENAI_BASE_URL'] = 'http://192.168.170.76:11434/v1'
 
 res = genai(model="qwen2.5:7b-instruct",
-            messages="tell me joke in 10 paragraphs")
+            messages="tell me joke in 5 lines")
 
 print(res)
--- test/dsollama_tool.py
+++ test/dsollama_tool.py
@@ -3,31 +3,13 @@ import os
 
 os.environ['OPENAI_BASE_URL'] = 'http://192.168.170.76:11434/v1'
 
-def current_weather(city:str) -> str:
+def get_weather(city:str) -> str:
     """Get the current weather for a city"""
     return f"its very cool 12 degrer in {city}"
 
-tools=[{
-  'type': 'function',
-  'function': {
-    'name': 'current_weather',
-    'description': 'Get the current weather for a city',
-    'parameters': {
-      'type': 'object',
-      'properties': {
-        'city': {
-          'type': 'string',
-          'description': 'The name of the city',
-        },
-      },
-      'required': ['city'],
-    },
-  },
-},
-]
 res = completion(model="llama3.2:3b-instruct-fp16",
             messages="tell me weather in hyderabad",
-            tools=[current_weather]
+            tools=[get_weather]
                  # tools=tools
                  )
 
